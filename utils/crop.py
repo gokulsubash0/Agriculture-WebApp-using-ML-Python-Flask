511adc5d8ce89c0c9bf7539b2823b7326d8533d2
gokulsubash0 <84393332+gokulsubash0@users.noreply.github.com>
2023-06-23 21:57:01 +0530

    Add files via upload

---
+++ utils/crop.py
@@ -0,0 +1,112 @@
+crop_dic = {
+
+    "['lentil']": """ <b>Crop</b>: Lentil <br/>
+    
+    <br/> 1.It's a Winter Crop.""",
+
+    "['muskmelon']": """ <b>Crop</b>: Muskmelon <br/>
+        
+        <br/> """,
+
+    "['jute']": """ <b>Crop</b>: Jute <br/>
+       
+        <br/>""",
+
+
+    "['coffee']": """ <b>Crop</b>: Coffee <br/>
+
+        <br/> """,
+
+
+    "['grapes']": """ <b>Crop</b>: Grapes <br/>
+
+        <br/>1.Crop which requires very High Ratio of Phosphorous Content in Soil.
+        <br/>2.Crop which requires very High Ratio of Potassium Content in Soil.
+        <br/>3.Grow at High and Low Temperatures. """,
+
+    
+    "['banana ']": """ <b>Crop</b>: Banana <br/>
+
+        <br/>""",
+
+    "['blackgram']": """ <b>Crop</b>: Blackgram <br/>
+
+        <br/>1.It's a Summer Crop.""",
+    
+    "['kidneybeans']": """ <b>Crop</b>: Kidneybeans <br/>
+
+        <br/>1.Crop which requires very Low Humidity.
+        <br/>""",
+    
+    "['maize']": """ <b>Crop</b>: Maize <br/>
+
+        <br/><br/>""",
+    
+    "['mothbeans']": """ <b>Crop</b>: Mothbeans <br/>
+
+        <br/>1.Grow at High and Low Ph.
+        <br/>2.It's a Summer Crop.""",
+    
+    "['orange ']": """ <b>Crop</b>: Orange <br/>
+
+        <br/><br/>""",
+    
+    "['pomegranate']": """ <b>Crop</b>: Pomergranate <br/>
+
+        <br/>1.It's a Winter Crop.
+        <br/>""",
+    
+    "['watermelon']": """ <b>Crop</b>: Watermelon<br/>
+
+        <br/><br/>""",
+    
+    "['cotton']": """ <b>Crop</b>: Cotton <br/>
+
+        <br/>1.Crop which requires very High Ratio of Nitrogen Content in Soil.
+        <br/> """,
+    
+    "['mango']": """ <b>Crop</b>: Mango <br/>
+
+        <br/>1.It's a Summer Crop.
+        <br/>""",
+    
+    "['coconut']": """ <b>Crop</b>: Coconut <br/>
+         
+       <br/>1.Crop which requires very High Rainfall.
+       <br/>1.It's a Rainy Crop.""",
+    
+    "['apple']": """ <b>Crop</b>: Apple <br/>
+        
+        <br/>1.Crop which requires very High Ratio of Phosphorous Content in Soil.
+        <br/>2.Crops which requires very High Ratio of Potassium Content in Soil.
+        <br/> """,
+
+    
+    "['chickpea']": """ <b>Crop</b>: Chickpea <br/>
+
+        <br/>Crop which requires very Low Humidity.
+        <br/> """,
+    
+    
+    "['papaya']": """ <b>Crop</b>: Papaya <br/>
+        
+        <br/>1.Crop which requires very High Rainfall.
+        <br/>2.Crop which requires very High Temperature.
+        <br/>3.It's a Rainy Crop.""",
+    
+    
+    "['pigeonpeas']": """ <b>Crop</b>: Pigeonpeas <br/>Disease: No disease<br/>
+
+        <br/><br/> """,
+    
+    
+    "['mungbean ']": """ <b>Crop</b>: Mungbean <br/>
+
+        <br/> """,
+
+
+    "['rice']": """ <b>Crop</b>: Rice <br/>
+         
+         <br/>1.Crop which requires very High Rainfall.
+         <br/>2.It's a Rainy Crop."""
+}
